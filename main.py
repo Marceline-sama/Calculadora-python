7885c5c60ec4980cfee9c3c2d0b54e06480e3950
sabuel <samuelhgsa@gmail.com>
2024-04-19 20:41:03 -0300

    Método de multiplicação

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
-from operacoes import operacoes
+from operacoes import Operacoes
 
-print("calculadora")
+def main():
+    print("Calculadora")
 
-operacao = operacoes()
+    operacao = Operacoes()
 
-op = int(input("""digite 1 para realizar uma soma de 2 numeros\n
-           digite 2 para realizar uma subtração de 2 numeros\n
-           digite 3 para realizar uma multiplicação de 2 numeros\n
-           digite 4 para realizar a divisão de 2 numeros: \n"""))
-if (op == 1):
-    num1 = int(input("digite o primeiro numero para calcular: "))
-    num2 = int(input("digite o primeiro numero para calcular: "))
-    operacao = operacoes()
-    resultado = operacoes.soma(num1, num2)
-    print("resultado da soma: ", resultado)
-    
-if (op == 2):
-    num1 = int(input("digite o primeiro numero para calcular: "))
-    num2 = int(input("digite o primeiro numero para calcular: "))
-    operacao = operacoes()
-    resultado = operacoes.sub(num1, num2)
-    print("resultado da ssubtação: ", resultado)
-    
-if (op == 3):
-    pass#metodo da multiplicação
-if (op == 4):
-    pass#metodo da divisão
+    op = int(input("""Digite 1 para realizar uma soma de 2 números\n
+            Digite 2 para realizar uma subtração de 2 números\n
+            Digite 3 para realizar uma multiplicação de 2 números\n
+            Digite 4 para realizar uma divisão de 2 números: \n"""))
+    if (op == 1):
+        num1 = int(input("Digite o primeiro número para calcular: "))
+        num2 = int(input("Digite o segundo número para calcular: "))
+        #operacao = Operacoes()
+        resultado = operacao.soma(num1, num2)
+        print("Resultado da soma: ", resultado)
+        
+    if (op == 2):
+        num1 = int(input("Digite o primeiro número para calcular: "))
+        num2 = int(input("Digite o segundo número para calcular: "))
+        #operacao = operacoes()
+        resultado = operacao.sub(num1, num2)
+        print("Resultado da subtração: ", resultado)
+        
+    if (op == 3):
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        resultado = operacao.multi(num1, num2)
+        print(f'A multiplicação de {num1} por {num2} é: {resultado}')
+    if (op == 4):
+        pass#metodo da divisão
 
+if __name__ == "__main__":
+    main()
